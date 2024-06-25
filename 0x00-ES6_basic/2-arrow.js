4d093990d469cb5c4df8f32682f44442977250cc
adoelhaoumy <alhoumi20@gmail.com>
2024-06-25 06:19:49 -0700

    mmA

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,19 @@
+export default function getNeighborhoodsList() {
+  /* eslint-disable */
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}export default function getNeighborhoodsList() {
+	  /* eslint-disable */
+	  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+	  const self = this;
+	  this.addNeighborhood = (newNeighborhood) => {
+		      self.sanFranciscoNeighborhoods.push(newNeighborhood);
+		      return self.sanFranciscoNeighborhoods;
+		    };
+}
